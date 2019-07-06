83123becc4797af7648130f60cfa510467165033
michaelvargas19 <michael.vargas@javeriana.edu.co>
2019-07-06 01:21:49 -0500

    VF

---
+++ jerseyServer/src/main/java/co/edu/javeriana/ws/entidades/Politicas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.javeriana.ws.entidades;
+import java.util.Date;
+/**
+ *
+ * @author USUARIO
+ */
+    public class Politicas {
+        private Date expiracion;
+        private String nombre;
+
+    public Politicas(Date expiracion, String nombre) {
+        this.expiracion = expiracion;
+        this.nombre = nombre;
+    }
+
+    public Date getExpiracion() {
+        return expiracion;
+    }
+
+    public void setExpiracion(Date expiracion) {
+        this.expiracion = expiracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    }
