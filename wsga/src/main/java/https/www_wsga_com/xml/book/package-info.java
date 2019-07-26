881bcbeb6e598b32cc5bee959783369d9c7675ca
William Moreno <williammorenoprieto@gmail.com>
2019-07-25 19:02:54 -0500

    Pruebas

--- wsga/src/main/java/https/www_wsga_com/xml/book/package-info.java
+++ wsga/src/main/java/https/www_wsga_com/xml/book/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.07.25 a las 05:22:22 PM COT 
+// Generado el: 2019.07.25 a las 06:38:42 PM COT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://www.wsga.com/xml/book", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
